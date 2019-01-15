42fb578f857af8df4bc18ca7a742579ad80478d8
FOLUWA <moronfoluwaakintola@gmail.com>
2019-01-15 18:48:58 +0100

    web.configs

--- keys.js
+++ keys.js
@@ -1,16 +1,10 @@
 // add this file to .gitignore
 
 module.exports = {
-    google: {
-        clientID:'482458351987-d9seu4j5oa0c1ca7d8iqhc6opc90sjaa.apps.googleusercontent.com',
-        clientSecret:'Gn4RGjFH6RsYcoGhq9tWIVJ0'
-    },
     mongodb:{
         dbURI: 'mongodb://wgbadmin:wgbadmin1@ds227594.mlab.com:27594/the-wgb'
     }
 };
-//DEPLOYING TO HEROKU
-//https://devcenter.heroku.com/articles/deploying-nodejs
 //mongodb://localhost/testForAuth
 //mongodb://wgbadmin:wgbadmin1@ds227594.mlab.com:27594/the-wgb
 
