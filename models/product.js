1eae033cc039d8172c0e9d87a3021f9630058e0c
Roqak <roqak1@gmail.com>
2019-01-26 01:21:41 +0100

    Ressolve Head Issues

--- models/product.js
+++ models/product.js
@@ -7,13 +7,8 @@ var schema = new Schema({
     title: {type: String, required: true},
     category: {type: String, required: true},
     price: {type: Number, required: true},
-<<<<<<< HEAD
     description: {type: String, required: true},
    // imagePath: {type: String, required: true}
-=======
-    description: {type: String, required: true}
-    // imagePath: {type: String, required: true}
->>>>>>> a439b0a35e08b418540f8311b7186442ad05ece9
 });
 
 module.exports = mongoose.model('Product', schema);
