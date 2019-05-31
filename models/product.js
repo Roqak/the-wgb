1274451d409416dcc76aadfd2b54ec794f1375e4
FOLUWA <moronfoluwaakintola@gmail.com>
2019-05-31 11:21:27 +0100

    Finished dashboard interface

--- models/product.js
+++ models/product.js
@@ -2,12 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({ 
-    userId: {type: String, required: true},
-    user : {type: Schema.Types.ObjectId,ref: 'Product'},
-    title: {type: String, required: true},
-    category: {type: String, required: true},
-    price: {type: Number, required: true},
-    description: {type: String, required: true}
+	user: {type: Schema.Types.ObjectId, ref: 'User'},
+    email: {type: String, required: true},
+    product_name: {type: String, required: true}, 
+    product_category: {type: String, required: true},
+    product_price: {type: Number, required: true},
+    product_description: {type: String, required: true},
+    status: {type: String, required: true}, 
+    state: {type: String, required: true}, 
+    lga: {type: String, required: true}, 
+    whatsapp_num: {type: String, required: true}, 
+    date_created: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Product', schema);
